ecff98a223231c88172bd266a06319469c7d23ab
EGT-FranciscoBorja <francisco.borja@egt.ec>
2025-04-02 16:26:57 -0500

    Add role restrictions on homepage

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,51 +1,56 @@
+'use client'
+
 import React from 'react'
-import { CardInfo } from '../types'
 import { GiShipWheel } from "react-icons/gi";
 import { FaHotel, FaUserShield } from "react-icons/fa6";
 import { PiSunHorizonBold } from "react-icons/pi";
 import Link from 'next/link'
+import { useAppSelector } from '@/app/hooks'
+import { RootState } from '@/app/lib/store'
 
-const cards_info = (): CardInfo[] => [
-  {
-    id: 1,
-    path: '/listHotels',
-    title: 'Hotels',
-    description: 'Availibility and prices for hotels around the world',
-    icon: <FaHotel />,
-  },
-  {
-    id: 2,
-    path: '/listCruises',
-    title: 'Cruises',
-    description: 'Availibility and prices for cruises around the world',
-    icon: <GiShipWheel />,
-  },
-  {
-    id: 3,
-    path: '/activities',
-    title: 'Activities',
-    description: 'Availibility and prices for activities in Ecuador',
-    icon: <PiSunHorizonBold />,
-  },
-  {
-    id: 4,
-    path: '/listUsers',
-    title: 'Users',
-    description: 'Users of the system',
-    icon: <FaUserShield />,
+const Cards = () => {
+  const user = useAppSelector((state: RootState) => state.auth.user)
+  const isSuperAdmin = Array.isArray(user?.roles) && user.roles.includes('super-admin')
 
-  },
-]
+  const cards = [
+    {
+      id: 1,
+      path: '/listHotels',
+      title: 'Hotels',
+      description: 'Availibility and prices for hotels around the world',
+      icon: <FaHotel />,
+    },
+    {
+      id: 2,
+      path: '/listCruises',
+      title: 'Cruises',
+      description: 'Availibility and prices for cruises around the world',
+      icon: <GiShipWheel />,
+    },
+    {
+      id: 3,
+      path: '/activities',
+      title: 'Activities',
+      description: 'Availibility and prices for activities in Ecuador',
+      icon: <PiSunHorizonBold />,
+    },
+    ...(isSuperAdmin ? [{
+      id: 4,
+      path: '/listUsers',
+      title: 'Users',
+      description: 'Users of the system',
+      icon: <FaUserShield />,
+    }] : [])
+  ]
 
-const cards = () => {
   return (
     <div className='w-full'>
       <div className='grid grid-cols-4 gap-4 sm:grid-cols-1 lg:grid-cols-4'>
-        {cards_info().map((card) => (
+        {cards.map((card) => (
           <Link
             key={card.id}
             href={card.path}
-            className='p-4 bg-white rounded-lg shadow-md text-center'
+            className='p-4 bg-white rounded-lg shadow-md text-center hover:shadow-lg transition-shadow'
           >
             <div className='flex items-center justify-center w-16 h-16 mx-auto mb-4 text-white bg-indigo-500 rounded-full'>
               {card.icon}
@@ -59,4 +64,4 @@ const cards = () => {
   )
 }
 
-export default cards
+export default Cards
